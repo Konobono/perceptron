ffbf2bf297b145fa12774e839da24244ddefce71
Konobono <konrad.pierzchlewicz@gmail.com>
2026-03-01 00:02:32 +0100

    Final version of the project.

--- perceptron.py
+++ perceptron.py
@@ -1,73 +1,115 @@
 import pandas as pd
 import numpy as np
-import csv
 
 data = pd.read_csv("data_banknote_authentication.csv", sep=",", decimal=".")
-# data.drop(data.columns[2], axis=1, inplace=True)
 data = data.replace('?', np.NaN)
 data = data.apply(pd.to_numeric)
-# data = data.sample(frac=1, random_state=42)
 data = data.reindex(np.random.permutation(data.index))
-# print(data)
-
 
 X = data.iloc[:, :-1].values
-X1 = np.array(X)
+X = np.array(X)
+X = np.c_[np.ones((len(X), 1)), X]
 y = data.iloc[:, -1].values
-y1 = np.array(y)
+y = np.array(y)
 
 n_samples = len(X)
-n_train = int(0.6 * n_samples)
-n_val = int(0.2 * n_samples)
-n_test = n_samples - n_train - n_val
+n_train = int(0.8 * n_samples)
+n_test = n_samples - n_train
+
+X_train = X[:n_train]
+y_train = y[:n_train]
+X_test = X[n_train:]
+y_test = y[n_train:]
 
 
-# zainicjowac wagi -> obliczyc wagi*input(x) W CALYM WIERSZU ->
-# za pomoca funkcji signum wyznaczyc klase 0 albo 1 jesli >1 lub <1
-# -> jesli predykcja = prawdziwa klasa to idziemy do x+1, jezeli nie edytujemy wagi i bias
+def activation(matrix):
+    if matrix >= 0:
+        return 1
+    else:
+        return 0
 
 
-def weights_matrix(data_x):
-    weights = np.random.uniform(low=-1.0, high=1.0, size=data_x.shape)
-    weights_array = np.array(weights)
-    # print(weights_array)
-    return weights_array
+def guess(data_x, weights):
+    suma = np.dot(weights, data_x)
+    y_p = activation(suma)
+    return y_p
 
 
-def sum_matrix(data_x):
-    suma = np.multiply(weights_matrix(data_x), data_x)
-    sum_wx = np.array(suma)
-    result = []
+def train(lr, epoch, weights):
+    for j in range(epoch):
+        for x_i, target in zip(X_train, y_train):
+            delta_w = lr * (target - guess(weights, x_i))  # lr * error i potem *input
+            for i in range(len(x_i)):
+                weights[i] += delta_w * x_i[i]
 
-    for row in sum_wx:
-        row_sum = sum(row)
-        result.append(row_sum)
-    return result
-
-
-def first_prediction(data_x):
-
-    y_pred = np.sign(sum_matrix(data_x))
-    a_2d = y_pred.reshape((-1, 1))
-    f = open('trans_matrix.csv', 'w')
-    writer = csv.writer(f)
-    writer.writerows(a_2d)
-    f.close()
-    y_pred_t = pd.read_csv("trans_matrix.csv", sep=",", decimal=".")
-    y_pred_t = y_pred_t.apply(pd.to_numeric)
-    return y_pred_t
+    print('Weights: \n', weights)
 
 
-def w_correction(data_x):
-    matrix = first_prediction(data_x)
-    for i in range(len(matrix)):
-        if matrix[i] == y[i]:
-            continue
-        else:
-            pass
+def test(weights, data_test):
+    pred_labels = []
+
+    for i in range(len(y_test)):
+        y_pred = guess(weights, data_test[i])
+        pred_labels.append(y_pred)
+
+    print("\nTest data: \n", X_test[:5])
+    print("\nPredicted labels: \n", pred_labels[:5])
+
+    return pred_labels
 
 
-print(first_prediction(X1))
+def confusion_matrix(true_label, pred_label):
+
+    num_classes = len(np.unique(true_label))
+    tp = np.zeros(num_classes)
+    fp = np.zeros(num_classes)
+    tn = np.zeros(num_classes)
+    fn = np.zeros(num_classes)
+    con_matrix = np.zeros((num_classes, num_classes))
+
+    for i in range(len(true_label)):
+        true_label_check = true_label[i]
+        pred_label_check = pred_label[i]
+        con_matrix[true_label_check - 1][pred_label_check - 1] += 1
+    print("\nConfusion matrix: \n", con_matrix)
+
+    for i in range(len(true_label)):
+        for j in range(num_classes):
+            if (true_label[i] == j) & (pred_label[i] == j):
+                tp[j] += 1
+            if (true_label[i] != j) & (pred_label[i] == j):
+                fp[j] += 1
+            if (true_label[i] != j) & (pred_label[i] != j):
+                tn[j] += 1
+            if (true_label[i] == j) & (pred_label[i] != j):
+                fn[j] += 1
+
+    sensitivity = np.zeros(num_classes, dtype=np.float64)
+    specificity = np.zeros(num_classes, dtype=np.float64)
+    precision = np.zeros(num_classes, dtype=np.float64)
+    accuracy = np.zeros(num_classes, dtype=np.float64)
+
+    for i in range(num_classes):
+        sensitivity[i] = tp[i] / (tp[i] + fn[i])
+        specificity[i] = tn[i] / (fp[i] + tn[i])
+        precision[i] = tp[i] / (tp[i] + fp[i])
+        accuracy[i] = (tp[i] + tn[i]) / len(true_label)
+
+    for k in range(num_classes):
+        print("\nTP instances for class", k, "=", tp[k])
+        print("TN instances for class", k, "=", tn[k])
+        print("FP instances for class", k, "=", fp[k])
+        print("FN instances for class", k, "=", fn[k])
+
+    for p in range(num_classes):
+        print("\nSensitivity for class", p, "=", sensitivity[p]*100, "%")
+        print("Specificity for class", p, "=", specificity[p]*100, "%")
+        print("Precision for class", p, "=", precision[p]*100, "%")
+        print("Accuracy for class", p, "=", accuracy[p]*100, "%")
 
 
-
+# w_i = np.random.uniform(low=0, high=1.0, size=5)
+w_i = np.zeros(5)
+train(lr=0.1, epoch=10, weights=w_i)
+predicted_label = test(w_i, X_test)
+confusion_matrix(y_test, predicted_label)
